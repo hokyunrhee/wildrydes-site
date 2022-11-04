87bd6a30cc08199bdfa85b3c987e8fe915b40d0b
hokyunrhee <hokyun.rhee@gmail.com>
2022-11-04 10:06:10 +0900

    feat: frontend 추가

---
+++ frontend/js/config.js
@@ -0,0 +1,11 @@
+window._config = {
+  cognito: {
+    userPoolId: "ap-northeast-2_h8gsb1wr2", // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: "12qkm66j3n2qlrdjluj8dto0mj", // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: "ap-northeast-2", // e.g. us-east-2
+  },
+  api: {
+    invokeUrl:
+      "https://bnonsscw28.execute-api.ap-northeast-2.amazonaws.com/dev/ride", // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
+};
