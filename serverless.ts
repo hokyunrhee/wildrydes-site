c9f190cb636a827f3d7fd5285443539901ff889e
hokyunrhee <hokyun.rhee@gmail.com>
2022-11-04 11:42:09 +0900

    feat: function 권한 추가

--- serverless.ts
+++ serverless.ts
@@ -20,6 +20,17 @@ const serverlessConfiguration: AWS = {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
     },
+    iam: {
+      role: {
+        statements: [
+          {
+            Action: "dynamodb:*",
+            Effect: "Allow",
+            Resource: { "Fn::GetAtt": ["RideTable", "Arn"] },
+          },
+        ],
+      },
+    },
   },
   functions,
   package: { individually: true },
